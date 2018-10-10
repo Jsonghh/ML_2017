ff8e6b86726201a89f2fad67cd50833bf839ed10
Jsonghh <jiesonghe@outlook.com>
2018-10-09 20:28:07 -0400

    Add data structures implementation

--- Assignment1_Predict_PM2.5/Predict_PM2.5.py
+++ Assignment1_Predict_PM2.5/Predict_PM2.5.py
@@ -1,3 +1,9 @@
+'''
+Given information of 18 polluntants(factors) in the air over 12*20 days,
+24 hours per day, train a model to predict the PM2.5 value based on the
+information in the previous 9 hours.
+'''
+
 
 
 import csv
